ab77cb652a0dd4c7a41c1aa65ad1b24e5acb9fff
Rian Kormos <rkormos@pcd-gwap3xy-lt.ucsf.edu>
2024-06-19 15:33:33 -0700

    Updated the definitions of the degrees of freedom.

--- helix_pair_analysis/analyze.py
+++ helix_pair_analysis/analyze.py
@@ -12,37 +12,38 @@ from utils import *
 def parse_args():
     """Parse command-line arguments."""
     parser = argparse.ArgumentParser(description='Analyze helix pair.')
-    parser.add_argument('pdb1', type=str, 
+    parser.add_argument('pdb1', type=str,
                         help='First PDB file containing the helix pair.')
-    parser.add_argument('selstr11', type=str, 
+    parser.add_argument('selstr11', type=str,
                         help=('ProDy selection string for the first helix '
                               'in the first PDB file.'))
-    parser.add_argument('selstr12', type=str, 
+    parser.add_argument('selstr12', type=str,
                         help=('ProDy selection string for the second helix '
                               'in the first PDB file.'))
-    parser.add_argument('pdb2', type=str, 
+    parser.add_argument('pdb2', type=str,
                         help='First PDB file containing the helix pair.')
-    parser.add_argument('selstr21', type=str, 
+    parser.add_argument('selstr21', type=str,
                         help=('ProDy selection string for the first helix '
                               'in the second PDB file.'))
-    parser.add_argument('selstr22', type=str, 
+    parser.add_argument('selstr22', type=str,
                         help=('ProDy selection string for the second helix '
                               'in the second PDB file.'))
     parser.add_argument('--window', type=int, default=7,
                         help=('Number of residues in the window for the '
-                              'analysis. Default is 7.'))
+                              'analysis. Must be odd, default is 7.'))
     args = parser.parse_args()
     return args
 
 
 def main(args):
     """Analyze the change in relative coordinates in a helix pair in two PDBs.
-    
+
     Parameters
     ----------
     args : argparse.Namespace
         Command-line arguments.
     """
+    assert args.window % 2, 'Window length must be odd.'
     CA_sel = ' and name CA'
     struct1 = pr.parsePDB(args.pdb1)
     helix1_1 = struct1.select(args.selstr11 + CA_sel).getCoords()
@@ -71,26 +72,30 @@ def main(args):
         dists_2 = cdist(helix2_1_window, helix2_2_window)
         idx21, idx22 = np.unravel_index(np.argmin(dists_2), dists_2.shape)
 
-        idx11, idx12, idx21, idx22 = 0, 0, 0, 0
+        idx11, idx12, idx21, idx22 = args.window // 2, \
+                                     args.window // 2, \
+                                     args.window // 2, \
+                                     args.window // 2
+        helix_half_length = 1.5 * args.window // 2
 
         # find rotation and translation matrices for each window
-        R11, t11, sse11, _ = kabsch(ideal_helix(args.window, start=-idx11), 
+        R11, t11, sse11, _ = kabsch(ideal_helix(args.window, start=-idx11),
                                     helix1_1_window)
         ideal_11 = ideal_helix(args.window, start=-idx11) @ R11 + t11
-        R12, t12, sse12, _ = kabsch(ideal_helix(args.window, start=-idx12), 
+        R12, t12, sse12, _ = kabsch(ideal_helix(args.window, start=-idx12),
                                     helix1_2_window)
         ideal_12 = ideal_helix(args.window, start=-idx12) @ R12 + t12
-        R21, t21, sse21, _ = kabsch(ideal_helix(args.window, start=-idx21), 
+        R21, t21, sse21, _ = kabsch(ideal_helix(args.window, start=-idx21),
                                     helix2_1_window)
         ideal_21 = ideal_helix(args.window, start=-idx21) @ R21 + t21
-        R22, t22, sse22, _ = kabsch(ideal_helix(args.window, start=-idx22), 
+        R22, t22, sse22, _ = kabsch(ideal_helix(args.window, start=-idx22),
                                     helix2_2_window)
         ideal_22 = ideal_helix(args.window, start=-idx22) @ R22 + t22
 
         '''
-        print(np.sqrt(sse11 / args.window), 
-              np.sqrt(sse12 / args.window), 
-              np.sqrt(sse21 / args.window), 
+        print(np.sqrt(sse11 / args.window),
+              np.sqrt(sse12 / args.window),
+              np.sqrt(sse21 / args.window),
               np.sqrt(sse22 / args.window))
         '''
 
@@ -101,23 +106,25 @@ def main(args):
         # t2 = t22 - t21 @ R2
 
         '''
-        sanity_check_3d_plot(struct1.select('name CA').getCoords(), 
-                             ideal_11, 
+        sanity_check_3d_plot(struct1.select('name CA').getCoords(),
+                             ideal_11,
                              ideal_12,
-                             R11, t11, 
-                             R12, t12,  
+                             R11, t11,
+                             R12, t12,
                              R1, t1)
-        sanity_check_3d_plot(struct2.select('name CA').getCoords(), 
-                             ideal_21, 
-                             ideal_22, 
-                             R21, t21, 
-                             R22, t22, 
+        sanity_check_3d_plot(struct2.select('name CA').getCoords(),
+                             ideal_21,
+                             ideal_22,
+                             R21, t21,
+                             R22, t22,
                              R2, t2)
         '''
 
         # calculate the effective coordinates for each transform
-        eff_coords_1.append(effective_coords(R11, t11, R12, t12))
-        eff_coords_2.append(effective_coords(R21, t21, R22, t22))
+        eff_coords_1.append(effective_coords(R11, t11, R12, t12,
+                                             helix_half_length))
+        eff_coords_2.append(effective_coords(R21, t21, R22, t22,
+                                             helix_half_length))
 
     eff_coords_1 = np.array(eff_coords_1).T
     eff_coords_2 = np.array(eff_coords_2).T
@@ -128,20 +135,18 @@ def main(args):
     #     gearbox1_diff, gearbox2_diff = eff_coords_1
     # '''
     dist_diff = eff_coords_1[0] - eff_coords_2[0]
-    angle_diff = wrapped_diff(eff_coords_1[1], eff_coords_2[1])
-    piston1_diff = eff_coords_1[2] - eff_coords_2[2]
-    piston2_diff = eff_coords_1[3] - eff_coords_2[3]
-    gearbox1_diff = wrapped_diff(eff_coords_1[4], eff_coords_2[4])
-    gearbox2_diff = wrapped_diff(eff_coords_1[5], eff_coords_2[5])
+    angle_diff = eff_coords_1[1] - eff_coords_2[1]
+    piston_diff = eff_coords_1[2] - eff_coords_2[2]
+    gearbox1_diff = 2.3 * wrapped_diff(eff_coords_1[3], eff_coords_2[3])
+    gearbox2_diff = 2.3 * wrapped_diff(eff_coords_1[4], eff_coords_2[4])
     # '''
 
     plt.figure()
     plt.plot(resnums, dist_diff, label='Distance difference (Angstroms)')
-    plt.plot(resnums, angle_diff, label='Angle difference (Radians)')
-    # plt.plot(resnums, piston1_diff, label='Piston 1 difference (Angstroms)')
-    plt.plot(resnums, piston2_diff, label='Piston difference (Angstroms)')
-    plt.plot(resnums, gearbox1_diff, label='Gearbox 1 difference (Radians)')
-    plt.plot(resnums, gearbox2_diff, label='Gearbox 2 difference (Radians)')
+    plt.plot(resnums, angle_diff, label='Scissor difference (Angstroms)')
+    plt.plot(resnums, piston_diff, label='Piston difference (Angstroms)')
+    plt.plot(resnums, gearbox1_diff, label='Gearbox 1 difference (Angstroms)')
+    plt.plot(resnums, gearbox2_diff, label='Gearbox 2 difference (Angstroms)')
     plt.legend(fontsize='large')
     plt.xlabel('Residue window', fontsize='x-large')
     plt.ylabel('Coordinate difference', fontsize='x-large')
--- helix_pair_analysis/utils.py
+++ helix_pair_analysis/utils.py
@@ -3,40 +3,41 @@ import numba as nb
 from matplotlib import pyplot as plt
 
 from scipy.optimize import minimize
+from scipy.spatial.distance import cdist
 
-def sanity_check_3d_plot(all_coords, window1, window2, 
+def sanity_check_3d_plot(all_coords, window1, window2,
                          R1, t1, R2, t2, R, t):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    ax.scatter(all_coords[:, 0], all_coords[:, 1], all_coords[:, 2], 
+    ax.scatter(all_coords[:, 0], all_coords[:, 1], all_coords[:, 2],
                color='b', marker='.')
-    ax.scatter(window1[:, 0], window1[:, 1], window1[:, 2], 
+    ax.scatter(window1[:, 0], window1[:, 1], window1[:, 2],
                color='r', marker='o')
-    ax.scatter(window2[:, 0], window2[:, 1], window2[:, 2], 
+    ax.scatter(window2[:, 0], window2[:, 1], window2[:, 2],
                color='g', marker='s')
     w_trans = window1 @ R + t
-    ax.scatter(w_trans[:, 0], w_trans[:, 1], w_trans[:, 2], 
+    ax.scatter(w_trans[:, 0], w_trans[:, 1], w_trans[:, 2],
                color='m', marker='o')
-    
+
     dist, angle, piston1, piston2, gearbox1, gearbox2 = \
         effective_coords(R, t @ R.T)
-    
+
     # plot helical axes
     piston1, piston2 = line_line_nearest_points(t1, R1[2], t2, R2[2])
-    axis1 = np.vstack([t1 + R1[2] * x for x in 
+    axis1 = np.vstack([t1 + R1[2] * x for x in
                        np.linspace(-piston1, piston1, 50)])
-    axis2 = np.vstack([t2 + R2[2] * x for x in 
+    axis2 = np.vstack([t2 + R2[2] * x for x in
                        np.linspace(-piston2, piston2, 50)])
     ax.plot(axis1[:, 0], axis1[:, 1], axis1[:, 2])
     ax.plot(axis2[:, 0], axis2[:, 1], axis2[:, 2])
     # plot line of closest approach
     right_pt_1 = t1 + R1[2] * piston1
     right_pt_2 = t2 + R2[2] * piston2
-    v = np.vstack([f * right_pt_1 + (1 - f) * right_pt_2 
+    v = np.vstack([f * right_pt_1 + (1 - f) * right_pt_2
                    for f in np.linspace(0, 1, 51)])
     u = right_pt_2 - right_pt_1
     u /= np.linalg.norm(u)
-    ax.plot(v[:, 0], v[:, 1], v[:, 2])   
+    ax.plot(v[:, 0], v[:, 1], v[:, 2])
 
     ax.set_xlim([-60, 60])
     ax.set_ylim([-60, 60])
@@ -46,7 +47,7 @@ def sanity_check_3d_plot(all_coords, window1, window2,
 
 def line_line_nearest_points(p1, u1, p2=None, u2=None):
     """Find the points at which two lines are closest.
-    
+
     Parameters
     ----------
     p1 : np.array [3] or [3 x 1]
@@ -61,10 +62,10 @@ def line_line_nearest_points(p1, u1, p2=None, u2=None):
     Returns
     -------
     t1 : float
-        Coefficient such that the nearest point on line 1 to line 2 is 
+        Coefficient such that the nearest point on line 1 to line 2 is
         p1 + t1 * u1.
     t2 : float
-        Coefficient such that the nearest point on line 1 to line 2 is 
+        Coefficient such that the nearest point on line 1 to line 2 is
         p2 + t2 * u2.
     """
     if p2 is None:
@@ -78,10 +79,10 @@ def line_line_nearest_points(p1, u1, p2=None, u2=None):
     return t1, t2
 
 
-def effective_coords(R1, t1, R2, t2):
-    """Calculate the effective coordinates of a helix pair with frames 
+def effective_coords(R1, t1, R2, t2, half_helix_length):
+    """Calculate the effective coordinates of a helix pair with frames
        (R1, t1) and (R2, t2).
-    
+
     Parameters
     ----------
     R1 : np.array [3 x 3]
@@ -92,57 +93,82 @@ def effective_coords(R1, t1, R2, t2):
         Rotation matrix for the frame of Helix 1.
     t2 : np.array [3]
         Translation vector for the frame of Helix 1.
+    half_helix_length : float
+        Half-length of each helix (in Angstroms).
 
     Returns
     -------
     eff_coords : np.array [6]
-        Array of effective coordinates for the helix pair.  These coordinates 
+        Array of effective coordinates for the helix pair.  These coordinates
         are as follows:
-            1. Distance between the helices at closest approach as if they 
+            1. Distance between the helices at closest approach as if they
                were infinitely long (in Angstroms).
             2. Angle between the helices at closest approach (in radians).
             3. Piston displacement of the first helix along its z-axis
                (in Angstroms).
-            4. Piston displacement of the second helix along its z-axis 
+            4. Piston displacement of the second helix along its z-axis
                (in Angstroms).
-            5. Gearbox rotation of the first helix about its z-axis 
+            5. Gearbox rotation of the first helix about its z-axis
                (in radians).
             6. Gearbox rotation of the second helix about its z-axis
                (in radians).
     """
-    # calculate the unit vector in the direction of the displacement between 
+    # calculate the unit vector in the direction of the displacement between
     # the helical axes at closest approach
     u = np.cross(R1[2], R2[2])
     u /= np.linalg.norm(u)
 
-    # compute the distance between the lines along the z-axis and the 
-    # R[2] axis at closest approach
-    dist = np.abs(np.dot(t2 - t1, u))
+    # compute the minimum distance between the line segments that run along
+    # each helix segment
+    denom = 1. - np.dot(R1[2], R2[2]) ** 2
+    lambda_1 = (np.dot(R1[2], t2 - t1) +
+                np.dot(R1[2], R2[2]) * np.dot(R2[2], t1 - t2)) / denom
+    lambda_2 = (np.dot(R2[2], t1 - t2) +
+                np.dot(R1[2], R2[2]) * np.dot(R1[2], t2 - t1)) / denom
+    factor_1 = np.min([half_helix_length, np.abs(lambda_1)]) / \
+               np.abs(lambda_1)
+    factor_2 = np.min([half_helix_length, np.abs(lambda_2)]) / \
+               np.abs(lambda_2)
+    displacement = t2 + lambda_2 * factor_2 * R2[2] - \
+                   t1 - lambda_1 * factor_1 * R1[2]
+    dist = np.linalg.norm(displacement)
 
-    # calculate the angle between the helical axes at closest approach
-    # by dotting the z-axis from the matrix R with the global z-axis 
-    # (i.e. R[2, 2])
-    angle = np.arccos(np.dot(R1[2], R2[2]))
+    # calculate the scissor degree of freedom, defined as the amount
+    # by which the maximum distance between the two line segments
+    # exceeds the minimum distance
+    endpoints = np.array([t1 + half_helix_length * R1[2],
+                          t1 - half_helix_length * R1[2],
+                          t2 + half_helix_length * R2[2],
+                          t2 - half_helix_length * R2[2]])
+    max_dist = cdist(endpoints[:2], endpoints[2:]).max()
+    scissor = max_dist - dist
 
-    # calculate the piston degrees of freedom, defined as the distance 
-    # between each helix frame and the point of nearest approach 
-    # between their axes
-    piston1, piston2 = line_line_nearest_points(t1, R1[2], t2, R2[2])
-    piston1, piston2 = np.mean([piston1, piston2]), piston1 - piston2
+    # calculate the piston degree of freedom, defined as the difference
+    # of the distance between each helix frame centroid and the point of
+    # nearest approach of the helix axes
+    piston = lambda_2 - lambda_1
 
-    # calculate the gearbox angles of each helix about its z-axis
-    # since u lies in either the xy-plane or the plane formed by 
-    # R[:, 0] and R[:, 1], compute the signed angle of u in each
-    gearbox1 = np.arctan2(np.dot(-u, R1[1]), np.dot(-u, R1[0]))
-    gearbox2 = np.arctan2(np.dot(-u, R2[1]), np.dot(-u, R2[0]))
+    # calculate the gearbox angles of each helix about its z-axis.
+    # since the inter-helix displacement vector lies in either the xy-plane
+    # or the plane formed by R[:, 0] and R[:, 1], compute the signed angle
+    # of the displacement in each
+    displacement_proj_1 = displacement - np.dot(displacement, R1[2]) * R1[2]
+    displacement_proj_2 = displacement - np.dot(displacement, R2[2]) * R2[2]
+    gearbox1 = -np.arctan2(np.dot(displacement_proj_1, R1[1]),
+                           np.dot(displacement_proj_1, R1[0]))
+    gearbox2 = -np.arctan2(np.dot(-displacement_proj_2, R2[1]),
+                           np.dot(-displacement_proj_2, R2[0]))
+    # without the minus sign, the arctan2 gives us the angle through which
+    # we'd need to rotate to get to the displacement vector from the local
+    # x-axis; we want to go the other way around, hence the minus sign
 
-    return dist, angle, piston1, piston2, gearbox1, gearbox2
+    return dist, scissor, piston, gearbox1, gearbox2
 
 
 def effective_coords_old(R, t):
-    """Calculate the effective coordinates of a helix pair with frames 
+    """Calculate the effective coordinates of a helix pair with frames
        that differ by the transformation (R, t).
-    
+
     Parameters
     ----------
     R : np.array [3 x 3]
@@ -153,41 +179,41 @@ def effective_coords_old(R, t):
     Returns
     -------
     eff_coords : np.array [6]
-        Array of effective coordinates for the helix pair.  These coordinates 
+        Array of effective coordinates for the helix pair.  These coordinates
         are as follows:
-            1. Distance between the helices at closest approach as if they 
+            1. Distance between the helices at closest approach as if they
                were infinitely long (in Angstroms).
             2. Angle between the helices at closest approach (in radians).
             3. Piston displacement of the first helix along its z-axis
                (in Angstroms).
-            4. Piston displacement of the second helix along its z-axis 
+            4. Piston displacement of the second helix along its z-axis
                (in Angstroms).
-            5. Gearbox rotation of the first helix about its z-axis 
+            5. Gearbox rotation of the first helix about its z-axis
                (in radians).
             6. Gearbox rotation of the second helix about its z-axis
                (in radians).
     """
-    # calculate the unit vector in the direction of the displacement between 
+    # calculate the unit vector in the direction of the displacement between
     # the helical axes at closest approach
     u = np.cross([0, 0, 1], R[2])
     u /= np.linalg.norm(u)
 
-    # compute the distance between the lines along the z-axis and the 
+    # compute the distance between the lines along the z-axis and the
     # R[2] axis at closest approach
     dist = np.abs(np.dot(t, u))
 
     # calculate the angle between the helical axes at closest approach
-    # by dotting the z-axis from the matrix R with the global z-axis 
+    # by dotting the z-axis from the matrix R with the global z-axis
     # (i.e. R[2, 2])
     angle = np.arccos(R[2, 2])
 
-    # calculate the piston degrees of freedom, defined as the distance 
-    # between each helix frame and the point of nearest approach 
+    # calculate the piston degrees of freedom, defined as the distance
+    # between each helix frame and the point of nearest approach
     # between their axes
     piston1, piston2 = line_line_nearest_points(t, R[2])
 
     # calculate the gearbox angles of each helix about its z-axis
-    # since u lies in either the xy-plane or the plane formed by 
+    # since u lies in either the xy-plane or the plane formed by
     # R[:, 0] and R[:, 1], compute the signed angle of u in each
     gearbox1 = np.arctan2(u[1], u[0])
     gearbox2 = np.arctan2(np.dot(-u, R[:, 1]), np.dot(-u, R[:, 0]))
@@ -197,7 +223,7 @@ def effective_coords_old(R, t):
 
 def effective_coords_old(R, t):
     """Calculate the effective coordinates of a helix pair with R and t.
-    
+
     Parameters
     ----------
     R : np.array [3 x 3]
@@ -208,43 +234,43 @@ def effective_coords_old(R, t):
     Returns
     -------
     eff_coords : np.array [6]
-        Array of effective coordinates for the helix pair.  These coordinates 
+        Array of effective coordinates for the helix pair.  These coordinates
         are as follows:
-            1. Distance between the helices at closest approach as if they 
+            1. Distance between the helices at closest approach as if they
                were infinitely long (in Angstroms).
             2. Angle between the helices at closest approach (in radians).
             3. Piston displacement of the first helix along its z-axis
                (in Angstroms).
-            4. Piston displacement of the second helix along its z-axis 
+            4. Piston displacement of the second helix along its z-axis
                (in Angstroms).
-            5. Gearbox rotation of the first helix about its z-axis 
+            5. Gearbox rotation of the first helix about its z-axis
                (in radians).
             6. Gearbox rotation of the second helix about its z-axis
                (in radians).
     """
-    # calculate the unit vector in the direction of the displacement between 
+    # calculate the unit vector in the direction of the displacement between
     # the helical axes at closest approach
     u = np.cross([0, 0, 1], R[2])
     u /= np.linalg.norm(u)
 
-    # compute the distance between the lines along the z-axis and the 
+    # compute the distance between the lines along the z-axis and the
     # R[2] axis at closest approach
     dist = np.abs(np.dot(t, u))
 
     # calculate the angle between the helical axes at closest approach
-    # by dotting the z-axis from the matrix R with the global z-axis 
+    # by dotting the z-axis from the matrix R with the global z-axis
     # (i.e. R[2, 2])
     angle = np.arccos(R[2, 2])
 
-    # calculate the piston degrees of freedom, defined as the distance 
-    # between each helix frame and the point of nearest approach 
+    # calculate the piston degrees of freedom, defined as the distance
+    # between each helix frame and the point of nearest approach
     # between their axes
     piston1, piston2 = line_line_nearest_points(t, R[2])
-    # print(dist, np.linalg.norm(np.array([0, 0, piston1]) - 
+    # print(dist, np.linalg.norm(np.array([0, 0, piston1]) -
     #                            t + piston2 * R[2]))
 
     # calculate the gearbox angles of each helix about its z-axis
-    # since u lies in either the xy-plane or the plane formed by 
+    # since u lies in either the xy-plane or the plane formed by
     # R[:, 0] and R[:, 1], compute the signed angle of u in each
     gearbox1 = np.arctan2(u[1], u[0])
     gearbox2 = np.arctan2(np.dot(-u, R[:, 1]), np.dot(-u, R[:, 0]))
@@ -254,7 +280,7 @@ def effective_coords_old(R, t):
 
 def wrapped_diff(a, b, wrap=np.pi):
     """Calculate the wrapped difference between two angles.
-    
+
     Parameters
     ----------
     a : float
@@ -262,9 +288,9 @@ def wrapped_diff(a, b, wrap=np.pi):
     b : float
         Second angle in radians.
     wrap : float, optional
-        The value at which the angle wraps around to the opposite of itself. 
+        The value at which the angle wraps around to the opposite of itself.
         Default is pi.
-        
+
     Returns
     -------
     diff : float
@@ -277,7 +303,7 @@ def wrapped_diff(a, b, wrap=np.pi):
 
 def ideal_helix(n_residues, rise=1.5, twist=100, radius=2.3, start=0):
     """Generate coordinates for an ideal helix.
-    
+
     Parameters
     ----------
     n_residues : int
@@ -289,36 +315,36 @@ def ideal_helix(n_residues, rise=1.5, twist=100, radius=2.3, start=0):
     radius : float, optional
         Radius of the helix in Angstroms. Default is 2.3.
     start : int, optional
-        Which residue to start the helix on. The residue with index 
+        Which residue to start the helix on. The residue with index
         0 will always lie along the x-axis.  Default is 0.
-        
+
     Returns
     -------
     coords : np.array [n_points x 3]
         Array of coordinates for the ideal helix.
     """
-    t = np.linspace(start * twist * np.pi / 180, 
-                    (n_residues + start) * twist * np.pi / 180, 
+    t = np.linspace(start * twist * np.pi / 180,
+                    (n_residues + start) * twist * np.pi / 180,
                     n_residues, endpoint=False)
     x = radius * np.cos(t)
     y = radius * np.sin(t)
-    z = np.linspace(0, n_residues * rise, n_residues, 
+    z = np.linspace(0, n_residues * rise, n_residues,
                     endpoint=False)
     return np.array([x, y, z]).T
 
 
 def kabsch(X, Y):
-    """Rotate and translate X into Y to minimize the SSD between the two, 
-       and find the derivatives of the SSD with respect to the entries of Y. 
-       
-       Implements the SVD method by Kabsch et al. (Acta Crystallogr. 1976, 
-       A32, 922) and the SVD differentiation method by Papadopoulo and 
+    """Rotate and translate X into Y to minimize the SSD between the two,
+       and find the derivatives of the SSD with respect to the entries of Y.
+
+       Implements the SVD method by Kabsch et al. (Acta Crystallogr. 1976,
+       A32, 922) and the SVD differentiation method by Papadopoulo and
        Lourakis (INRIA Sophia Antipolis. 2000, research report no. 3961).
 
     Parameters
     ----------
     X : np.array [N x 3]
-        Array of mobile coordinates to be transformed by a proper rotation 
+        Array of mobile coordinates to be transformed by a proper rotation
         to minimize sum squared displacement (SSD) from Y.
     Y : np.array [N x 3]
         Array of stationary coordinates against which to transform X.
@@ -326,10 +352,10 @@ def kabsch(X, Y):
     Returns
     -------
     R : np.array [3 x 3]
-        Proper rotation matrix required to transform X such that its SSD 
+        Proper rotation matrix required to transform X such that its SSD
         with Y is minimized.
     t : np.array [3]
-        Translation matrix required to transform X such that its SSD with Y 
+        Translation matrix required to transform X such that its SSD with Y
         is minimized.
     ssd : float
         Sum squared displacement after alignment.
@@ -357,7 +383,7 @@ def kabsch(X, Y):
            np.einsum('im,mjkl->ijkl', np.dot(U, D), dVtdH)
     dRdY = np.einsum('km,ijml->ijkl', Xc, dRdH)
     XdRdY = np.einsum('im,mjkl->ijkl', Xc, dRdY)
-    d_ssd_dY = 2. * (np.sum(XRmY * XdRdY, axis=(0, 1)) - XRmY) 
+    d_ssd_dY = 2. * (np.sum(XRmY * XdRdY, axis=(0, 1)) - XRmY)
     return R, t, ssd, d_ssd_dY
 
 
@@ -377,10 +403,10 @@ def populate_omegas(U, S, Vt):
     Returns
     -------
     omega_U : np.array [3 x 3 x 3 x 3]
-        omega_U matrix of matrices as described in Papadopolou and 
+        omega_U matrix of matrices as described in Papadopolou and
         Lourakis (2000).
     omega_Vt : np.array [3 x 3 x 3 x 3]
-        omega_V matrix of matrices as described in Papadopolou and 
+        omega_V matrix of matrices as described in Papadopolou and
         Lourakis (2000), but with the last two dimensions transposed.
     """
     omega_U = np.zeros((3, 3, 3, 3))
@@ -388,9 +414,9 @@ def populate_omegas(U, S, Vt):
     for i in range(3):
         for j in range(3):
             for k, l in [(0, 1), (1, 2), (2, 0)]:
-                system_A = np.array([[S[l], S[k]], 
+                system_A = np.array([[S[l], S[k]],
                                      [S[k], S[l]]])
-                system_b = np.array([[U[i, k] * Vt[l, j]], 
+                system_b = np.array([[U[i, k] * Vt[l, j]],
                                      [-U[i, l] * Vt[k, j]]])
                 if S[k] != S[l]:
                     soln = np.linalg.solve(system_A, system_b)
